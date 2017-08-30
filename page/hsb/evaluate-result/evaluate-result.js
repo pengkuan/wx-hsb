628699d203358b17819fc4c651dcaf2130fe449a
MartinHan <hanhan145@163.com>
2017-08-30 16:18:40 +0800

    update mail order

--- page/hsb/evaluate-result/evaluate-result.js
+++ page/hsb/evaluate-result/evaluate-result.js
@@ -5,24 +5,28 @@ const products = require('../modules/model/products.js');
 let that;
 Page({
     data: {
-        evaluatePrice : '',
-        modelname : '',
-        answerArray : [],
-        isExpanded : false
+        evaluatePrice: '',
+        modelname: '',
+        answerArray: [],
+        isExpanded: false,
+        itemid: {},
+        selected: {}
     },
     onLoad: function (options) {
         that = this;
         wx.setNavigationBarTitle({
             title: options.name
         });
-        
+
         this.setData({
-            evaluatePrice : options.evaluatePrice,
-            modelname : options.name
+            evaluatePrice: options.evaluatePrice,
+            modelname: options.name
         });
         this.initQuestionArr();
         let selected = options.selected
-        products.evaluate(options.itemid,selected,(data) => {
+        this.data.itemid = options.itemid;
+        this.data.selected = options.selected;
+        products.evaluate(options.itemid, selected, (data) => {
             that.setData({
                 evaluatePrice: data.quotation
             })
@@ -30,14 +34,19 @@ Page({
 
     },
 
-    initQuestionArr : function() {
+    initQuestionArr: function () {
         wx.getStorage({
-            key : constant.LOCAL_OPTION_KEY,
-            success : (res) => {
+            key: constant.LOCAL_OPTION_KEY,
+            success: (res) => {
                 that.setData({
-                    answerArray : res.data
+                    answerArray: res.data
                 });
             }
+        });
+    },
+    onRecycleBtnClick: function (event) {
+        wx.navigateTo({
+            url: "../mail-method/mail-method?itemid=" + this.data.itemid + "&selects=" + this.data.selected
         });
     },
 
