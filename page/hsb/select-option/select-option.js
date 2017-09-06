2c34d4a5d68bb413c72cb704a31f083a8972d907
MartinHan <hanhan145@163.com>
2017-09-06 11:17:42 +0800

    update multi selected

--- page/hsb/select-option/select-option.js
+++ page/hsb/select-option/select-option.js
@@ -11,18 +11,19 @@ Page({
         modelname: 'modelname',
         selectOptions: [],
         allOptions: [],
+        allTransOptions: [],
         nowSelectIndex: 0,
         itemid: 0,
         evaluateEnable: false,
         scrollToView: {},
-        wHeight : 800,
-        choosePercent : 0,
+        wHeight: 800,
+        choosePercent: 0,
     },
     onLoad: function (options) {
         that = this;
         selectResultMap.clear();
         wx.setNavigationBarTitle({
-            title: options.name
+            title: '评估价格'
         })
 
         wx.getSystemInfo({
@@ -40,14 +41,8 @@ Page({
 
         products.getSelectOption(this.data.itemid, (response) => {
             that.data.allOptions = response.itemList;
-            let i = 0;
-            for (let item of that.data.allOptions) {
-                that.data.selectOptions.push(JSON.parse(JSON.stringify(item)));
-                if (i > that.data.nowSelectIndex) {
-                    that.data.selectOptions[i].question = [];
-                }
-                i++;
-            }
+            that.data.allTransOptions = that.transMultOptions(response.itemList);
+            that.data.selectOptions = that.transNowOptions(that.data.allTransOptions);
             that.setData({
                 selectOptions: that.data.selectOptions
             });
@@ -55,9 +50,48 @@ Page({
 
     },
 
+    transNowOptions : function(allTransOptions) {
+        let result = [];
+        let i = 0;  
+        for (let item of allTransOptions) {
+            let newItem = JSON.parse(JSON.stringify(item));
+            result.push(newItem);
+            if (i > that.data.nowSelectIndex) {
+                result[i].question = [];
+            }
+            i++;
+        }
+        return result;
+    },
+
+    transMultOptions: function (allOptions) {
+        let i = 0;
+        let allTransOptions = [];
+        let multiItem = new Object();
+        multiItem.isMulti = true;
+        multiItem.question = [];
+        multiItem.name = '功能性选项(可多选或不选)';
+        for (let item of allOptions) {
+            let newItem = JSON.parse(JSON.stringify(item));
+            if(item.conftype == '3') {
+                for(let questionItem of newItem.question) {
+                    if(questionItem.show == '1') {
+                        multiItem.question.push(questionItem);
+                    }
+                }
+            } else {
+                    
+                allTransOptions.push(newItem);
+            }
+            
+        }
+        allTransOptions.push(multiItem);
+        return allTransOptions;
+    },
+
     refreshSelectOptionsBySelectIndex: function () {
         let i = 0;
-        for (let item of that.data.allOptions) {
+        for (let item of that.data.allTransOptions) {
             if (i <= that.data.nowSelectIndex) {
                 this.data.selectOptions[i].question = item.question;
             }
@@ -79,11 +113,20 @@ Page({
     updateSelectOptions: function () {
         let selectOptions = this.data.selectOptions;
         let allSelected = true;
-        for (let questionItem of selectOptions) {
-            let id = questionItem.id;
+        for (let item of selectOptions) {
+            let id = item.id;
+            if(item.isMulti) {
+                let selectAnswerId = selectResultMap.get(id);
+                for (let answerItem of item.question) {
+                    if (selectAnswerId == answerItem.id) {
+                        answerItem.isSelected = !answerItem.isSelected;
+                    }
+                }
+                continue;
+            }
             if (selectResultMap.has(id)) {
                 let selectAnswerId = selectResultMap.get(id);
-                for (let answerItem of questionItem.question) {
+                for (let answerItem of item.question) {
                     if (selectAnswerId == answerItem.id) {
                         answerItem.isSelected = true;
                     } else {
@@ -99,14 +142,14 @@ Page({
         if (allSelected) {
             that.onAllOptionSelected();
         }
-        let percent = parseInt(that.data.nowSelectIndex / 1.0 / that.data.allOptions.length * 100);
-        if(that.data.choosePercent < percent) {
+        let percent = parseInt(that.data.nowSelectIndex / 1.0 / that.data.allTransOptions.length * 100);
+        if (that.data.choosePercent < percent) {
             that.data.choosePercent = percent;
         }
         that.setData({
             selectOptions: selectOptions,
             scrollToView: "option_" + that.data.nowSelectIndex,
-            choosePercent :  that.data.choosePercent
+            choosePercent: that.data.choosePercent
         });
     },
 
@@ -117,7 +160,7 @@ Page({
     },
 
     onEvaluateBtnClicked: function () {
-        if(!that.data.evaluateEnable) {
+        if (!that.data.evaluateEnable) {
             return;
         }
         let answersArray = that.getSelectQuestionArray();
